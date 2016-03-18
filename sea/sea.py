2d2207368609357d1818ab94c0d5feff563ac09b
sergk <zaswed76@gmail.com>
2016-03-18 18:27:25 +0200

    create sea

--- sea/sea.py
+++ sea/sea.py
@@ -9,8 +9,8 @@ class Cell:
         self.y = y
         self.x = x
         self.shooting = False  # стреляли ли в эту клетку
-        self._distance_to_obstacles_x = self._max - self.x
-        self._distance_to_obstacles_y = self._max - self.y
+        self._distance_to_obstacles_x = self._max - (self.x - 1)
+        self._distance_to_obstacles_y = self._max - (self.y - 1)
 
     @property
     def distance_to_obstacles_x(self):
@@ -18,7 +18,7 @@ class Cell:
 
     @distance_to_obstacles_x.setter
     def distance_to_obstacles_x(self, obstacles):
-        self._distance_to_obstacles_x = self._max - obstacles
+        self._distance_to_obstacles_x = self._max - (obstacles - 1)
 
     @property
     def distance_to_obstacles_y(self):
@@ -26,9 +26,32 @@ class Cell:
 
     @distance_to_obstacles_y.setter
     def distance_to_obstacles_y(self, obstacles):
-        self._distance_to_obstacles_y = self._max - obstacles
+        self._distance_to_obstacles_y = self._max - (obstacles - 1)
+
+    def __repr__(self):
+        return '({}, {})'.format(self.y, self.x)
 
 
 class Sea(list):
- def __init__(self, width, height): 
+    def __init__(self):
         super().__init__()
+
+
+    def create_field(self, width, height):
+        for y in range(height):
+            line = []
+            for x in range(width):
+                line.append((Cell(x, y)))
+            self.append(line)
+
+class Ship(list):
+    def __init__(self, bow, course, deck):
+        super().__init__()
+
+if __name__ == '__main__':
+    sea = Sea()
+    sea.create_field(10, 10)
+    print(sea[0][6].distance_to_obstacles_x)
+    sea[0][6].distance_to_obstacles_x = 8
+    print(sea[0][6].distance_to_obstacles_x)
+
