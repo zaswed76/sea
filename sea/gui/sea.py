4b92307638b3eed8b7c1486fa67b25eba1d4d0d4
serg <zaswed76@gmail.com>
2016-03-22 21:17:58 +0200

    ошибка вычисления пустого пространства

--- sea/gui/sea.py
+++ sea/gui/sea.py
@@ -5,6 +5,11 @@
 import sys
 from PyQt5 import QtWidgets
 
+class Cell(QtWidgets.QLabel):
+    def __init__(self, *__args):
+        super().__init__(*__args)
+
+
 class Sea(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -14,7 +19,7 @@ class Sea(QtWidgets.QWidget):
     def create_grid(self, w, h):
         for x in range(w):
             for y in range(h):
-                self.box.addWidget(QtWidgets.QPushButton(str((x, y))), x, y)
+                self.box.addWidget(QtWidgets.QPushButton(str((x, y))), y, x)
 
 
 
@@ -24,6 +29,6 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     # app.setStyleSheet(open('./etc/{0}.qss'.format('style'), "r").read())
     main = Sea()
-    main.create_grid(10, 10)
+    main.create_grid(5, 10)
     main.show()
     sys.exit(app.exec_())
