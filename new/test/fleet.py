d913986b80339c3845ea8d1cf5b8cda8f9a68b73
cassa <zaswed76@gmail.com>
2017-03-07 07:42:31 +0200

    create sea

---
+++ new/test/fleet.py
@@ -0,0 +1,10 @@
+
+from collections import Collection
+
+class Fleet(Collection):
+    def __init__(self, names=None):
+        if names is not None:
+            self.__names = names
+
+
+
---
+++ new/test/ship.py
@@ -0,0 +1,40 @@
+from collections import Set, Sequence, MutableSet
+
+
+class Ship(Set):
+    Horizontal = "horizontal"
+    Vertical = "vertical"
+    def __init__(self, bow, size, vector):
+        self.vector = vector
+        self.size = size
+        self.bow = bow
+
+        if vector == Ship.Horizontal:
+            self._seq = set(x for x in range(bow, bow + size))
+        elif vector == Ship.Vertical:
+            assert (bow + (size - 1) * 10) < 100
+            self._seq = set(x for x in range(bow, (bow + size * 10), 10))
+
+    def __contains__(self, item):
+        return item in self._seq
+
+    def __iter__(self):
+        return iter(self._seq)
+
+    def __len__(self):
+        return len(self._seq)
+
+    def __repr__(self):
+        return "{}".format(self._seq)
+
+    def __add__(self, other):
+        self._seq.add(other)
+        return self._seq
+
+
+if __name__ == '__main__':
+    ship = Ship(1, 3, Ship.Vertical)
+    print(ship)
+    print(ship.bow)
+    print(len(ship))
+    print(ship.vector)
