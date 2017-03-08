f07a7eb3c64d7bfed57e4a56bda0f6e891c19922
cassa <zaswed76@gmail.com>
2017-03-08 12:12:51 +0200

    ship add  around

--- new/test/ship.py
+++ new/test/ship.py
@@ -10,44 +10,61 @@ class Ship(Set):
         self.vector = vector
         self.size = size
         self.bow = bow
-        self.around = set()
+        self._ship = set()
+        self._around = set()
+        self._all = set()
 
         if vector == Ship.Horizontal:
-            self._seq = set(x for x in range(bow, bow + size))
+            self._ship.update(set(x for x in range(bow, bow + size)))
         elif vector == Ship.Vertical:
             assert (bow + (size - 1) * 10) < 100
-            self._seq = set(x for x in range(bow, (bow + size * 10), 10))
+            self._ship.update(set(x for x in range(bow, (bow + size * 10), 10)))
+
+        self._set_around_ship()
+        self._set_all()
+
+    @property
+    def ship(self):
+        return self._ship
+
+    @property
+    def around(self):
+        return self._around
+
+    @property
+    def all(self):
+        return self._all
 
     def __contains__(self, item):
-        return item in self._seq
+        return item in self._ship
 
     def __iter__(self):
-        return iter(self._seq)
+        return iter(self._ship)
 
     def __len__(self):
-        return len(self._seq)
+        return len(self._ship)
 
     def __repr__(self):
-        return "{}".format(self._seq)
+        return "{}".format(self._ship)
 
     def __add__(self, other):
-        self._seq.add(other)
-        return self._seq
+        self._ship.add(other)
+        return self._ship
 
-    def _around_cell(self, cell):
+    def _set_around_cell(self, cell):
         m = [-1, 1, -10, 10, -11, -9, 9, 11]
         a = set(cell + x for x in m)
         return a
 
-    def around_ship(self):
+    def _set_around_ship(self):
         a = set()
-        for s in self._seq:
-            a.update(self._around_cell(s))
-        a -= self._seq
-        return set(x for x in a if Ship.Max >= x >= Ship.Min)
+        for s in self._ship:
+            a.update(self._set_around_cell(s))
+        a -= self._ship
+        self._around.update(set(x for x in a if Ship.Max >= x >= Ship.Min))
 
-
-
+    def _set_all(self):
+        self._all.update(self.ship.union(self.around))
 
 
 if __name__ == '__main__':
@@ -56,4 +73,7 @@ if __name__ == '__main__':
     # print(ship.bow)
     # print(len(ship))
     # print(ship.vector)
-    print(ship.around_ship())
+    opt = ["bow", "vector", "size", "ship", "around", "all"]
+    for v in opt:
+        print("{} = {}".format(v, getattr(ship, v)))
+
