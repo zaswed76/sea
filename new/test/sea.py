cc6d663d203b587ebd73e88d6be114d25f6055db
cassa <zaswed76@gmail.com>
2017-03-13 11:09:21 +0200

    ship add  around

--- new/test/sea.py
+++ new/test/sea.py
@@ -1,6 +1,7 @@
 
 from collections import Set, Sequence, MutableSequence, Hashable, UserDict
 import numpy
+from new.test.fleet import Fleet
 
 
 
@@ -9,8 +10,8 @@ class Cell:
     Max = 10
     Empty = "0"
     Ship = "8"
-    Wound = "x"
-    Around = "-"
+    Wound = "X"
+    Around = "*"
     def __init__(self, y, x):
         assert (y < Cell.Max and x < Cell.Max) and (y >= Cell.Min and x >= Cell.Min)
         self.coord = (y, x)
@@ -59,17 +60,58 @@ class Cell:
 class Ship(UserDict):
     Horizontal = "horizontal"
     Vertical = "vertical"
-    def __init__(self, sea, bow, size):
+    def __init__(self, sea, bow, size, vector):
         super().__init__()
         self.sea = sea
+        self.ship = self.data
+        self.around = {}
+        self.all = {}
         y, x = bow
-        self.data.update({(y, x): sea[(y, x)] for x in range(x, x + size)})
-        for k, c in self.data.items():
-            c.status = Cell.Ship
+        if vector == Ship.Horizontal:
+            self.data.update({(y, x): sea[(y, x)] for x in range(x, x + size)})
+        elif vector == Ship.Vertical:
+            self.data.update({(y, x): sea[(y, x)] for y in range(y, y + size)})
+
+        self.__mx = [-1, 1, -1, 0, 1, -1, 0, 1]
+        self.__my = [0, 0, -1, -1, -1, 1, 1, 1]
+
+        self._set_around_ship()
+        self._set_all()
+        self._set_status()
+
+    def _set_status(self):
+        for k, c in self.all.items():
+            if k in self.ship:
+                c.status = Cell.Ship
+            elif k in self.around:
+                c.status = Cell.Around
+
+
+
+    def _get_around_cell(self, cell):
+        y, x = cell
+        a = set(((y + _y), (x + _x)) for _y, _x in zip(self.__my, self.__mx))
+        return a
+
+    def _set_around_ship(self):
+        for name in self.data:
+            around_cell = self._get_around_cell(name)
+            for k in around_cell:
+                if k in self.sea:
+                    if not k in self.ship:
+                        self.around[k] = self.sea[k]
+
+    def _set_all(self):
+        self.all.update(self.ship)
+        self.all.update(self.around)
+
+
+
 class Sea(UserDict):
     def __init__(self):
         super().__init__()
         self.data.update({(y, x):Cell(y, x) for y in range(10) for x in range(10)})
+        self.fleet = Fleet()
 
     def __str__(self):
         lst = [x.status for x in self.data.values()]
@@ -77,7 +119,7 @@ class Sea(UserDict):
         return str(r)
 
     def create_ship(self, bow, size, vector):
-        return Ship(self, bow, size)
+        return Ship(self, bow, size, vector)
 
 
 
@@ -87,6 +129,10 @@ class Sea(UserDict):
 if __name__ == '__main__':
     sea = Sea()
     ship = sea.create_ship((5, 5), 4, Ship.Vertical)
+    sea.fleet.add_ship(ship)
+    x = (5, 5)
+    if x in sea.fleet:
+        sea[x].status = Cell.Wound
     print(sea)
 
 
