111a14415e22a3c568f007e19b464b54645289bf
serg <zaswed76@gmail.com>
2017-03-28 21:53:54 +0300

    ship add  around

--- game/test.py
+++ game/test.py
@@ -1,24 +1,8 @@
 
+from random import randint, choice
+from itertools import product, permutations
 
 
-import sys
-from PyQt5 import QtWidgets
-
-class Cell:
-    def __init__(self, parent=None, x=0, y=0):
-        self.y = y
-        self.x = x
-        print(y - x)
-
-
-class Widget(QtWidgets.QLabel, Cell):
-    def __init__(self, parent, x, y):
-        super().__init__()
-        self.resize(500, 500)
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    # app.setStyleSheet(open('./etc/{0}.qss'.format('style'), "r").read())
-    main = Widget(None, 1, 1)
-    main.show()
-    sys.exit(app.exec_())
+mx = [-1, 1, -1, 0, 1, -1, 0, 1]
+my = [0, 0, -1, -1, -1, 1, 1, 1]
+print(list(permutations((0, 1, -1), 2)))
